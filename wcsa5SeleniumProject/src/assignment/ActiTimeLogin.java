8a92a1da2b49df121f9a9b43004e9d40141592bd
Omkar Jagtap <Omkar Jagtap@LAPTOP-BK4MAGLA>
2023-07-22 18:06:58 +0530

    commit

---
+++ wcsa5SeleniumProject/src/assignment/ActiTimeLogin.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ActiTimeLogin {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.get("http://127.0.0.1/administration/administration.do");
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("manager");
+		driver.findElement(By.xpath("//a[text()='Login']")).click();
+	}
+}
